53edbcbe22a55cf19e1f3fdab1a1ccfdb6ab8fa4
Anjan Roy <anjanroy@yandex.com>
2020-07-05 20:12:39 +0530

    initial commit, started fviz

---
+++ fviz/__init__.py
@@ -0,0 +1,8 @@
+'''
+    fviz - Facebook data visualiser
+
+    A venture to visualise your downloaded facebook data,
+    depicting your activities overtime.
+'''
+
+__version__ = '0.1.0'
---
+++ fviz/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from __future__ import annotations
+
+
+def main():
+    print('Under active development ... :)')
+
+
+if __name__ == '__main__':
+    main()
