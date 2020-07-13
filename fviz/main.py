73920f9ecd11b84cbf4316260c87315742cd35e2
Anjan Roy <anjanroy@yandex.com>
2020-07-13 21:25:31 +0530

    parses command line args

--- fviz/main.py
+++ fviz/main.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
 
 from __future__ import annotations
+from argparse import ArgumentParser
+from typing import Tuple
+from os.path import exists, abspath
+
+
+def _getCMD() -> Tuple[str, str]:
+    '''
+        Parses command line args, passed while invoking script
+    '''
+    parser = ArgumentParser()
+    parser.add_argument('src',
+                        type=str,
+                        help='Exported compressed Facebook data as zip file')
+    parser.add_argument('sink',
+                        type=str,
+                        help='Sink directory path, where plots to be placed')
+    args = parser.parse_args()
+
+    if not (args.src and args.sink):
+        return None, None
+    if not (args.src.endswith('.zip') and exists(args.src)):
+        return None, None
+
+    return tuple(map(lambda e: abspath(e), [args.src, args.sink]))
 
 
 def main():
