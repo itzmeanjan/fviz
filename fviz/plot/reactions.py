62f9a32caeb020b1dd5c74db8f837b580df58bc2
Anjan Roy <anjanroy@yandex.com>
2020-07-13 11:51:58 +0530

    changed plot labels, saving `tight` bar plot

--- fviz/plot/reactions.py
+++ fviz/plot/reactions.py
@@ -10,26 +10,22 @@ def plotReactionCount(data: Dict[str, int], title: str, sink: str) -> bool:
     '''
         Given data holding reaction count for each type of reactions
         plotting it as vertical bar plot, while setting given title,
-        and exporting to given file
+        and exporting to given sink file
     '''
     if not data:
         return False
 
     try:
-        _y = list(data.keys())
-        _x = list(data.values())
-
         fig = plt.Figure(figsize=(16, 9), dpi=100)
 
         sns.set(style='darkgrid')
-        sns.barplot(x=_x, y=_y, orient='v')
-        plt.xlabel('Percentage of Occurance')
+        sns.barplot(x=list(data.keys()), y=list(data.values()), orient='v')
+        plt.xlabel('Reactions')
+        plt.ylabel('#-of Occurances')
         plt.title(title)
 
-        fig.tight_layout()
-        fig.savefig(sink, bbox_inches='tight', pad_inches=.5)
-
-        plt.close(fig=fig)
+        plt.savefig(sink, bbox_inches='tight', pad_inches=.5)
+        plt.close(fig)
         return True
     except Exception:
         return False
