4671428f5f4afa5bc52f0a6b63f950d962a042ad
Anjan Roy <anjanroy@yandex.com>
2020-07-13 11:34:52 +0530

    using seaborn for plotting reaction type to count - as bar plot

--- fviz/plot/reactions.py
+++ fviz/plot/reactions.py
@@ -3,9 +3,15 @@
 from __future__ import annotations
 from typing import Dict
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 
-def plotReactionCount(data: Dict[str, float], title: str, sink: str) -> bool:
+def plotReactionCount(data: Dict[str, int], title: str, sink: str) -> bool:
+    '''
+        Given data holding reaction count for each type of reactions
+        plotting it as vertical bar plot, while setting given title,
+        and exporting to given file
+    '''
     if not data:
         return False
 
@@ -13,20 +19,17 @@ def plotReactionCount(data: Dict[str, float], title: str, sink: str) -> bool:
         _y = list(data.keys())
         _x = list(data.values())
 
-        with plt.style.context('Solarize_Light2'):
-            fig = plt.Figure(figsize=(24, 12), dpi=100)
+        fig = plt.Figure(figsize=(16, 9), dpi=100)
 
-            plt.barh(range(len(_y)), _x, align='center',
-                     color='deepskyblue', lw=1.6)
+        sns.set(style='darkgrid')
+        sns.barplot(x=_x, y=_y, orient='v')
+        plt.xlabel('Percentage of Occurance')
+        plt.title(title)
 
-            plt.yticks(ticks=range(len(_y)), labels=_y)
-            plt.xlabel('Percentage of Occurance')
-            plt.title(title)
+        fig.tight_layout()
+        fig.savefig(sink, bbox_inches='tight', pad_inches=.5)
 
-            # fig.tight_layout()
-            fig.savefig(sink, bbox_inches='tight', pad_inches=.5)
-
-            plt.close(fig=fig)
+        plt.close(fig=fig)
         return True
     except Exception:
         return False
