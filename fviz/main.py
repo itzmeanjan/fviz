6162bb129ca36670b996ab9982e9bd0774b0a2ba
Anjan Roy <anjanroy@yandex.com>
2020-07-14 08:21:14 +0530

    added one more cmd arg, needs to ask for extract location for zip

--- fviz/main.py
+++ fviz/main.py
@@ -4,25 +4,10 @@ from __future__ import annotations
 from argparse import ArgumentParser
 from typing import Tuple
 from os.path import exists, abspath
-from os import mkdir
+from .extract import makeDir
 
 
-def _makeDir(target: str) -> bool:
-    '''
-        Creates directory at specified path
-        if not existing already, denotes 
-        success of operation by returning boolean value
-    '''
-    try:
-        if exists(target):
-            return True
-        mkdir(target)
-        return True
-    except Exception:
-        return False
-
-
-def _getCMD() -> Tuple[str, str]:
+def _getCMD() -> Tuple[str, str, str]:
     '''
         Parses command line args, passed while invoking script
     '''
@@ -30,21 +15,31 @@ def _getCMD() -> Tuple[str, str]:
     parser.add_argument('src',
                         type=str,
                         help='Exported compressed Facebook data as zip file')
+    parser.add_argument('extractAt',
+                        type=str,
+                        help='Extraction location of zip')
     parser.add_argument('sink',
                         type=str,
                         help='Sink directory path, where plots to be placed')
     args = parser.parse_args()
 
-    if not (args.src and args.sink):
-        return None, None
-    if not (args.src.endswith('.zip') and exists(args.src) and _makeDir(abspath(args.sink))):
-        return None, None
+    if not (args.src and args.extractAt and args.sink):
+        return None, None, None
+    if not (args.src.endswith('.zip') and exists(args.src) and makeDir(abspath(args.sink))):
+        return None, None, None
 
-    return tuple(map(lambda e: abspath(e), [args.src, args.sink]))
+    return tuple(map(lambda e: abspath(e), [args.src, args.extractAt, args.sink]))
 
 
 def main():
-    print('Under active development ... :)')
+    try:
+        src, extractAt, sink = _getCMD()
+        if not (src and extractAt and sink):
+            raise Exception('Bad CMD args')
+    except KeyboardInterrupt:
+        print('\n[!] Terminated')
+    except Exception as e:
+        print('[!] {}'.format(e))
 
 
 if __name__ == '__main__':
