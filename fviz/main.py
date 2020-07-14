d571abd9395de9da57d38774d90611342938a168
Anjan Roy <anjanroy@yandex.com>
2020-07-14 08:12:23 +0530

    creates sink directory if not existing already

--- fviz/main.py
+++ fviz/main.py
@@ -4,6 +4,22 @@ from __future__ import annotations
 from argparse import ArgumentParser
 from typing import Tuple
 from os.path import exists, abspath
+from os import mkdir
+
+
+def _makeDir(target: str) -> bool:
+    '''
+        Creates directory at specified path
+        if not existing already, denotes 
+        success of operation by returning boolean value
+    '''
+    try:
+        if exists(target):
+            return True
+        mkdir(target)
+        return True
+    except Exception:
+        return False
 
 
 def _getCMD() -> Tuple[str, str]:
@@ -21,7 +37,7 @@ def _getCMD() -> Tuple[str, str]:
 
     if not (args.src and args.sink):
         return None, None
-    if not (args.src.endswith('.zip') and exists(args.src)):
+    if not (args.src.endswith('.zip') and exists(args.src) and _makeDir(abspath(args.sink))):
         return None, None
 
     return tuple(map(lambda e: abspath(e), [args.src, args.sink]))
