46734cf367bca9ab0cfa5f7faa86103504be2d1b
Anjan Roy <anjanroy@yandex.com>
2020-07-16 08:52:12 +0530

    activity heatmap of user, generates a single image showing all activites over time for that user, seperated by years ( in each 365 days plotted )

--- fviz/plot/reactions.py
+++ fviz/plot/reactions.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 from __future__ import annotations
-from typing import Dict
+from typing import Dict, List, Tuple
 from matplotlib import pyplot as plt
 import seaborn as sns
+from ..model.reactions import Reactions
+from datetime import date, timedelta
+from math import ceil
 
 
 def plotReactionCount(data: Dict[str, int], title: str, sink: str) -> bool:
@@ -55,6 +58,91 @@ def plotPeerToReactionCount(data: Dict[str, int], title: str, sink: str) -> bool
         plt.close(fig)
         return True
     except Exception:
+        return False
+
+
+def _prepareHeatMapData(reactions: Reactions) -> Tuple[List[List[int]], List[date], List[str]]:
+    '''
+        Generates heatmap data, depicting user activity on facebook over whole period of time
+        i.e. on which date he/ she put which reaction on a facebook post how many number of times
+    '''
+    _dates = []
+
+    _start, _end = [i.date() for i in reactions.getTimeFrame]
+    while _start <= _end:
+        _dates.append(_start)
+        _start += timedelta(hours=24)
+
+    _reactionTypes = sorted(list(reactions.reactionTypes))
+    _groupedByDate = reactions.dateToReactionTypeAndCount
+
+    buffer = [[] for i in _reactionTypes]
+
+    for i, k in enumerate(_reactionTypes):
+        _tmp = buffer[i]
+
+        for j in _dates:
+            _tmp.append(_groupedByDate.get(j, {}).get(k, 0))
+
+    return buffer, _dates, _reactionTypes
+
+
+def plotReactionsOverTimeAsHeatMap(data: Reactions, title: str, sink: str) -> bool:
+    def _stripData(_frm: int, _to: int):
+        return [i[_frm: _to] for i in _buffer], _dates[_frm: _to]
+
+    if not data:
+        return False
+
+    try:
+        _buffer, _dates, _reactionTypes = _prepareHeatMapData(data)
+        _start = 0
+        _end = 365
+
+        fig, _axes = plt.subplots(
+            ceil(len(_dates) / 365),
+            1,
+            figsize=(100, 5 * (ceil(len(_dates) / 365) + 10)),
+            dpi=100)
+
+        for i in _axes:
+
+            _tmpBuffer, _tmpDates = _stripData(_start, _end)
+
+            sns.heatmap(
+                _tmpBuffer,
+                cmap='YlGnBu',
+                lw=.5,
+                ax=i)
+
+            i.set_xticklabels(
+                [k.strftime('%d %b, %Y') for k in _tmpDates],
+                rotation=90)
+            # i.tick_params(axis='x', which='major', labelsize=6)
+            i.set_yticklabels(
+                _reactionTypes,
+                rotation=0
+            )
+            i.set_title(
+                '{} [ {} - {} ]'.format(
+                    title,
+                    *[k.strftime('%d %b, %Y') for k in [_tmpDates[0], _tmpDates[-1]]]
+                )
+            )
+
+            _start = _end
+            _end += 365
+
+        fig.savefig(
+            sink,
+            bbox_inches='tight',
+            pad_inches=.5
+        )
+        plt.close(fig)
+
+        return True
+    except Exception as e:
+        print(e)
         return False
 
 
