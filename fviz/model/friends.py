7f3676af22778a0890948106fb54cc1416d4a2ea
Anjan Roy <anjanroy@yandex.com>
2020-07-20 20:38:16 +0530

    added code comment, obtain friend by index

--- fviz/model/friends.py
+++ fviz/model/friends.py
@@ -8,6 +8,12 @@ from .friend import Friend
 
 
 class Friends:
+    '''
+        Data holder class for all friends,
+        with their corresponding name
+        and time when they became friend
+    '''
+
     def __init__(self, friends: List[Friend]):
         self._friends = friends
 
@@ -15,8 +21,22 @@ class Friends:
     def friends(self) -> List[Friend]:
         return self._friends
 
+    def getByIndex(self, idx: int) -> Friend:
+        '''
+            Returns friend by index of holding list
+        '''
+        if not (idx >= 0 and idx < len(self.friends)):
+            return None
+
+        return self.friends[idx]
+
     @staticmethod
     def fromJSON(src: str) -> Friends:
+        '''
+            Given path to JSON data file, returns 
+            object holding all friends with their name
+            and time when they became friend
+        '''
         _obj = None
         try:
 
