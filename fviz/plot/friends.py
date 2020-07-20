06de1d09db56d4250fdcb7949910f2302d51f385
Anjan Roy <anjanroy@yandex.com>
2020-07-20 23:42:19 +0530

    changed line plot style, plotting using different data on different axes

--- fviz/plot/friends.py
+++ fviz/plot/friends.py
@@ -20,10 +20,10 @@ def _prepareDataForPlottingMonthlyFriendsCreated(friends: Friends) -> Tuple[List
     _start, _end = friends.getTimeFrame
     _x = []
     while _start <= _end:
-        _x.append(_start.strftime('%b, %Y'))
-        _start += timedelta(days=30)
+        if _start.strftime('%b, %Y') not in _x:
+            _x.append(_start.strftime('%b, %Y'))
 
-    _x = list(set(_x))
+        _start += timedelta(days=30)
 
     _buffer = friends.monthToFriendCount
     _y = [_buffer.get(i, 0) for i in _x]
@@ -42,7 +42,7 @@ def plotMonthlyFriendsCreated(friends: Friends, title: str, sink: str) -> bool:
         sns.set(style='darkgrid')
 
         _fig, _axes = plt.subplots(
-            ceil(len(_x)/12), 1, figsize=(16, ceil(len(_x)/12) * 8), dpi=100)
+            ceil(len(_x)/12), 1, figsize=(36, ceil(len(_x)/12) * 8), dpi=100)
 
         _start = 0
         _end = 12
@@ -51,10 +51,14 @@ def plotMonthlyFriendsCreated(friends: Friends, title: str, sink: str) -> bool:
             _tmpX = _x[_start: _end]
 
             sns.lineplot(
-                x=_tmpX,
+                x=range(len(_tmpX)),
                 y=_y[_start: _end],
                 ax=i
             )
+
+            i.set_xticks(range(len(_tmpX)))
+            i.set_xticklabels(_tmpX)
+            i.lines[0].set_linestyle('--')
 
             i.set_xlabel('Time')
             i.set_ylabel('#-of Friends Created')
@@ -65,6 +69,9 @@ def plotMonthlyFriendsCreated(friends: Friends, title: str, sink: str) -> bool:
                     _tmpX[-1]
                 )
             )
+
+            _start = _end
+            _end += 12
 
         _fig.savefig(
             sink,
