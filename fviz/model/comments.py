566db4c1aa163d991d4e77d4718f66a74f489934
Anjan Roy <anjanroy@yandex.com>
2020-07-22 10:57:00 +0530

    from Comments obj from JSON

--- fviz/model/comments.py
+++ fviz/model/comments.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 from typing import List
+from os.path import exists
+from json import load
 from .comment import Comment
 
 
@@ -30,6 +32,33 @@ class Comments:
         return self._comments[_idx]\
             if _idx >= 0 and _idx < self.count\
             else None
+
+    @staticmethod
+    def fromJSON(src: str) -> Comments:
+        '''
+            Given a JSON data file, holding all comments by this
+            facebook user, we'll form a Comments obj, for performing several interesting ops
+            on it
+        '''
+        if not exists(src):
+            return None
+
+        _buffer = []
+
+        with open(src, mode='r') as fd:
+            _buffer = load(fd)
+        if 'comments' not in _buffer:
+            return None
+
+        _comments = Comments([])
+
+        for i in _buffer['comments']:
+            _comments._comments.append(
+                Comment(i['title'],
+                        i['timestamp'],
+                        i.get('data', [])))
+
+        return _comments
 
 
 if __name__ == '__main__':
