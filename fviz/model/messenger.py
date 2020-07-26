21659dab27feca4a93a205bd3e56a83cd5d74018
Anjan Roy <anjanroy@yandex.com>
2020-07-26 09:30:30 +0530

    build object from json files

--- fviz/model/messenger.py
+++ fviz/model/messenger.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 from .messages import Messages
-from typing import List
+from typing import List, Dict, Any
+from json import load
+from functools import reduce
 
 
 class Messenger:
@@ -19,6 +21,27 @@ class Messenger:
 
     def byIndex(self, _idx: int) -> Messages:
         return self._inbox[_idx] if _idx >= 0 and _idx < self.count else None
+
+    @staticmethod
+    def fromJSON(src: List[str]) -> Messenger:
+        '''
+           Reads each JSON file content, holding messages
+           and objectifies them, finally forming Messenger object,
+           which can be manipulated later
+        '''
+
+        def _getFileContent(path: str) -> Dict[str, Any]:
+            _content = None
+            with open(path, mode='r') as fd:
+                _content = load(fd)
+            return _content
+
+        return Messenger(
+            reduce(
+                lambda acc, cur: acc +
+                [Messages.fromJSON(_getFileContent(cur))],
+                src,
+                []))
 
 
 if __name__ == '__main__':
