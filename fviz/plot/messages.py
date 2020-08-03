459d87427f0f82efd2e58b563e079b6430359550
Anjan Roy <anjanroy@yandex.com>
2020-08-03 09:09:51 +0530

    plotting function : plots least contributed ( by YOU ) private facebook chats as grouped bar chart

--- fviz/plot/messages.py
+++ fviz/plot/messages.py
@@ -126,8 +126,87 @@ def plotTopXPrivateChatsWithHighestContributonFromYou(messenger: Messenger, x: i
             plt.close(fig)
 
         return True
-    except Exception as e:
-        print(e)
+    except Exception:
+        return False
+
+
+def _prepareDataForChatThreadsWithLowestContributonFromYou(messenger: Messenger, x: int, participant: str) -> Tuple[List[str], List[float], List[str], List[str]]:
+    '''
+        Preparing data to be plotted as grouped bar plot, 
+        for private facebook chat threads where you've lowest contribution
+    '''
+    _data = messenger.topXPrivateChatsWithLowestContributionFromParticipant(
+        x,
+        participant)
+
+    _x = list(chain.from_iterable(
+        [['Least Contributed : {}'.format(i+1)] * 2 for i in range(x)]))
+    _y = list(chain.from_iterable([i[2:] for i in _data]))
+    _hue = list(chain.from_iterable(
+        [['Peer : 1', 'Peer : 2'] for i in range(x)]))
+    _names = list(chain.from_iterable([i[:2] for i in _data]))
+
+    return _x, _y, _hue, _names
+
+
+def plotPrivateChatThreadsWithLowestContributonFromYou(messenger: Messenger, x: int, participant: str, title: str, sink: str) -> bool:
+    '''
+        Plotting private facebook chat threads, where this user is having lowest contribution,
+        in terms of # -of messages transacted, as grouped bar plot, where each pair of labels
+        along X axis denote a chat happened between them, where left one is this facebook user, and
+        other one is his/ her chat peer.
+    '''
+    if not messenger:
+        return False
+
+    try:
+        _x, _y, _hue, _names = _prepareDataForChatThreadsWithLowestContributonFromYou(
+            messenger,
+            x,
+            participant)
+
+        with plt.style.context('dark_background'):
+            fig = plt.Figure(
+                figsize=(24, 9),
+                dpi=100)
+
+            sns.barplot(
+                x=_x,
+                y=_y,
+                hue=_hue,
+                palette='Oranges',
+                ax=fig.gca())
+
+            _names = list(
+                chain.from_iterable(
+                    zip(*[_names[i:i+2] for i in range(0, x * 2, 2)])))
+
+            for j, k in enumerate(fig.gca().patches):
+                fig.gca().text(k.get_x() + k.get_width() / 2,
+                               k.get_y() + k.get_height() * .2 + .1,
+                               _names[j],
+                               ha='center',
+                               rotation=90,
+                               fontsize=10,
+                               color='black')
+
+            fig.gca().set_ylim(0, 100)
+            fig.gca().set_xlabel('Least Contributed Facebook Chats',
+                                 labelpad=12)
+            fig.gca().set_ylabel('Percentage of Participation in Chat',
+                                 labelpad=12)
+            fig.gca().set_title(title,
+                                pad=16,
+                                fontsize=20)
+
+            fig.savefig(
+                sink,
+                bbox_inches='tight',
+                pad_inches=.5)
+            plt.close(fig)
+
+        return True
+    except Exception:
         return False
 
 
